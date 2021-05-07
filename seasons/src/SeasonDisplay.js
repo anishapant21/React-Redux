817c1767139cfc205ccc3cca602b24f986a70a59
eternal-moon <anishapant1@gmail.com>
2021-05-07 18:31:50 +0545

    add files

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const SeasonDisplay = () =>{
-    return <div> SeasonDisplay</div>
+//configuration object
+const seasonConfig = {
+    summer : {
+        text: "Let's hit the beach!",
+        iconName: 'sun'
+    },
+    winter : {
+        text: 'Burr it is cold!',
+        iconName: 'snowflake'
+    }
+};
 
+const getSeason =(lat, month) =>{
+    if (month > 2 && month<9) {
+        return lat>0 ? 'summer' : 'winter' ;
+    } else {
+        return lat > 0? 'winter' : 'summer' ;
+    }
+
+}
+
+const SeasonDisplay = (props) =>{
+    const season=getSeason(props.lat, new Date(). getMonth());
+    const {text, iconName } = seasonConfig[season]; //{text, iconName}
+
+    //use {} to put js logic on jsx
+    return ( 
+        <div> 
+            <i className={`${iconName} icon`} />
+            <h1>{ text} </h1>
+            <i className={`${iconName} icon`} />
+        </div>
+    );
 };
 
 export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
 
-    constructor(props){
-        super(props);
+    //constructor(props){
+      //  super(props);
 
-        this.state ={lat : null, errorMessage: '' };
+        //this.state ={lat : null, errorMessage: '' };
 
+        
+    //} we can use the direct initialization as below instead of this, works same
+
+    state ={lat : null, errorMessage: '' };
+
+    //component lifecycle method- better we use this way
+
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition( //getting location from geolocation
-            position => {
-                //to update our state we use setState
-                this.setState({ lat: position.coords.latitude});
-                console.log(position.coords.latitude);
-            }, 
-            (err) => {
-                this.setState({errorMessage: err.message});
-            }
+            position => this.setState({ lat: position.coords.latitude}), //to update our state we use setState
+            (err) => this.setState({errorMessage: err.message})
+            
             );
+
     }
 
     //React says we have to render
@@ -30,9 +35,9 @@ class App extends React.Component {
            }
         
         if (!this.state.errorMessage && this.state.lat){
-            return(
-                <div> Latitude : {this.state.lat}</div>
-            );
+            return <SeasonDisplay lat={this.state.lat}/> //passing as props to the child componenet
+                
+            
             }
         return <div> Loading...</div>;
         }
